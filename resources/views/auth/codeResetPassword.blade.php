a76dc4f077e4afabf998df55fc54c20351860c0b
himaaaintake40 <mohamedebrahimabbas@gmail.com>
2020-11-10 22:57:34 +0200

    handle syntax2&bootstrap

--- resources/views/auth/codeResetPassword.blade.php
+++ resources/views/auth/codeResetPassword.blade.php
@@ -12,7 +12,7 @@
 
     <!-- Stylesheet
 ============================================= -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('js/bootstrap/css/bootstrap.min.css'') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('js/bootstrap/css/bootstrap.min.css') }}" />
     <link rel="stylesheet" type="text/css" href= "{{ asset('css/loginStyle.css') }}" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
