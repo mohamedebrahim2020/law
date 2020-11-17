423fee28c0e1c936f95eb592e7290413d3f15ad6
himaaaintake40 <mohamedebrahimabbas@gmail.com>
2020-11-17 22:51:17 +0200

    handle migration2

--- database/migrations/2020_11_16_123026_create_projects_table.php
+++ database/migrations/2020_11_16_123026_create_projects_table.php
@@ -15,7 +15,7 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->string('project_name')->unique();
+            $table->string('project_name');
             $table->date('start_date');
             $table->string('branch_name');
             $table->integer('project_type')->nullable();
